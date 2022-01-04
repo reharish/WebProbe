c0cc3fbfcbefafdcbedcd6b39be0ff0229ba4202
lpaulsonraja <lpaulsonraja@gmail.com>
2022-01-04 18:18:57 +0530

    Added code for threading in webprobe.

--- webprobe.py
+++ webprobe.py
@@ -1,25 +1,65 @@
-import requests
 from sys import argv
-import re
+import threading
+import requests
 
+no_threads = 5
 arguments = ["--thread", "-t", "-o", "--output"]
 
-if argv[1] in arguments:
-    if argv[1] == "--thread" or argv[1] == "-t":
-      if int(argv[2]) < 120 :
-          thread = int(argv[2])
-      else:
-          print("Invalid Thread")
+"""
+Usage:
+python3 webprobe.py <SampleFile> 
+python3 webprobe.py --thread <NoOfThreads> <SampleFile>
+"""
+length_of_arguments = len(argv)
+file_name = argv[length_of_arguments -  1]
 
-def TheMain():
-    httpfile = open(argv[1],'rt')
-    for line in httpfile:
-        line = line.split('\n');
-        line = line[0]
-        if re.search("^http*",line):
-            req = requests.get(line)
+if length_of_arguments > 1:
+    if argv[1] in arguments:
+        if argv[1] == "--thread" or argv[1] == "-t":
+            if int(argv[2]) <= 120 :
+                no_threads = int(argv[2])
+            else:
+                print("Enter threads below 120.\n")
+                exit(120)
+        else:
+            print("Enter the arguments with file.\n")
+            exit(1)
+elif length_of_arguments == 1:
+    print("Pass arguments.\n")
+    exit(2)
+        
+"""
+Reading the file containing domain names.
+"""
+with open(file_name) as f:
+    lines = f.readlines()
+    lines = [line.rstrip() for line in lines]
+
+    
+def do_request():
+    for line in lines:
+        req = requests.get(line, verify=False)
         if req.ok:
+            print(line, end = ' ')
             print(200)
-            print(line)
-    httpfile.close()
+        else:
+            print("Oops unable to connect.\n")
+
+"""
+Connecting the domains from file using threads.
+"""
+threads = []
+for i in range(no_threads):
+    t = threading.Thread(target=do_request)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(no_threads):
+    threads[i].start()
+
+for i in range(no_threads):
+    threads[i].join()
+
+
+exit(0)
 
