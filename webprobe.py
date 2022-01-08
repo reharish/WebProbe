8312c577a26c048a9b2a719e2276762b202fd0bc
reharish <rengarajharish@gmail.com>
2022-01-08 18:21:08 +0000

    lint init

--- webprobe.py
+++ webprobe.py
@@ -6,38 +6,40 @@ import requests
 from requests.packages import urllib3
 urllib3.disable_warnings()
 
-no_threads = 5
-arguments = ["--thread", "-t", "-o", "--output"]
+def arguments():
+    no_threads = 5
+    arguments = ["--thread", "-t", "-o", "--output"]
 
-# Usage:
-# python3 webprobe.py <SampleFile> 
-# python3 webprobe.py --thread <NoOfThreads> <SampleFile>
+    # Usage:
+    # python3 webprobe.py <SampleFile> 
+    # python3 webprobe.py --thread <NoOfThreads> <SampleFile>
 
-length_of_arguments = len(argv)
-file_name = argv[length_of_arguments -  1]
+    length_of_arguments = len(argv)
+    file_name = argv[length_of_arguments -  1]
 
 
-if length_of_arguments > 1:
-    if argv[1] in arguments:
-        if argv[1] == "--thread" or argv[1] == "-t":
-            if int(argv[2]) <= 120 :
-                no_threads = int(argv[2])
+    if length_of_arguments > 1:
+        if argv[1] in arguments:
+            if argv[1] == "--thread" or argv[1] == "-t":
+                if int(argv[2]) <= 120 :
+                    no_threads = int(argv[2])
+                else:
+                    print("Enter threads below 120.\n")
+                    exit(120)
             else:
-                print("Enter threads below 120.\n")
-                exit(120)
-        else:
-            print("Enter the arguments with file.\n")
-            exit(1)
-elif length_of_arguments == 1:
-    print("Pass arguments.\n")
-    exit(2)
+                print("Enter the arguments with file.\n")
+                exit(1)
+    elif length_of_arguments == 1:
+        print("Pass arguments.\n")
+        exit(2)
 
-"""
-Reading the file containing domain names.
-"""
-with open(file_name) as f:
-    lines = f.readlines()
-    lines = [line.rstrip() for line in lines]
+    """
+    Reading the file containing domain names.
+    """
+    with open(file_name) as f:
+        lines = f.readlines()
+        lines = [line.rstrip() for line in lines]
+
     
 
 def do_request():
@@ -51,14 +53,14 @@ def do_request():
         except :
             pass
 
+
 # Connecting the domains from file using threads.
-
-
 
 if __name__!='__main':
     #do_request()
     print("Webprobe Init..")
 else:
+    arguments()
     threads = []
     for i in range(no_threads):
         t = threading.Thread(target=do_request)
