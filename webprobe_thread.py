5649c0275b3f99f1ef617f4a31f144e190528206
gokulkumar.m <gokulkumar.m@zilogic.com>
2022-04-25 13:57:58 +0530

    added multi_threading

--- webprobe_thread.py
+++ webprobe_thread.py
@@ -1,7 +1,7 @@
 import threading
 import requests
 import argparse
-
+from time import sleep
 """
 ArgParse to getting input
 """
@@ -17,29 +17,23 @@ Supressing warning caused by requests
 requests.packages.urllib3.disable_warnings()
 
 def do_request(url):
-    response = requests.get(url, verify=False, allow_redirects=False, timeout=5)
-    if response.ok:
-        print(url)
+    if not url: return
+    try:
+        response = requests.get(url, verify=False, allow_redirects=False, timeout=5)
+        print(url) if response.ok else print(f"response: {response.status_code} url: {url}")
+    except Exception as e:
+        pass
         
 
-def process_file(fname, t):
-    n_threads = []
+def process_file(fname, t=5):
     fp = open(fname,'rt')
-    while True:
-        line=fp.readline().strip()
-        if not line:
-            break
-        req = threading.Thread(target=do_request, args=(line,))
-        if len(n_threads) == t:
-            while True:
-                for i in n_threads:
-                    if not i.is_alive():
-                        i = req
-                        break
-        else:
-             n_threads.append(req)
-             req.start()
-        req.join()
+    arr = list(map(lambda a : a.strip(), fp.readlines()))
+    for each in arr:
+        req = threading.Thread(target=do_request, args=(each,))
+        while threading.active_count() >= t:
+            sleep(1) #every one sec we are checking thread count
+        req.start(), req.join()
+
 
 if __name__=="__main__":
     process_file(args.filename, args.thread)
